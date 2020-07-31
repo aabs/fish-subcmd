902aa893bcdf7a7540d56efd6be8bc3cedfea984
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-31 11:12:29 +1000

    wip

--- functions/__fd2_define_help_subcommand.fish
+++ functions/__fd2_define_help_subcommand.fish
@@ -1,8 +1,8 @@
 function __fd2_define_help_subcommand -a prefix_name
-    set -l ev $prefix_name"_help"
-    __fd2_define_subcommand -p $prefix_name -c "help" -e $ev -d "Display help for command $prefix_name"
+    set -l fun $prefix_name"_help"
+    __fd2_define_subcommand -p $prefix_name -c "help" -f $fun -d "Display help for command $prefix_name"
 
     # create an event handler for displaying the help for this command
-    eval 'function '$ev' ; __fd2_display_command_help_for '$prefix_name'; end'
+    eval 'function '$fun' ; __fd2_display_command_help_for '$prefix_name'; end'
 end
 
