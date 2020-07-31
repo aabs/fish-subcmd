0592244f834d516391e28cd274b5f8c46cf01f4f
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-31 10:55:13 +1000

    wip

--- functions/__fd2_define_subcommand_completion.fish
+++ functions/__fd2_define_subcommand_completion.fish
@@ -1,25 +1,25 @@
-function __fd2_define_subcommand_completion -a prefix subcmd summary
+function __fd2_define_subcommand_completion 
     set -l command_name ''
     set -l prefix ''
-    set -l desc ''
+    set -l description ''
 
     getopts $argv | while read -l key value
         switch $key
             case p prefix
-                set prefix_name $value
+                set prefix $value
             case c command_name
                 set command_name $value
-            case d desc
-                set summary $value
+            case d description
+                set description $value
         end
     end
 
-    if test -z $prefix_name
+    if test -z $prefix
       echo "prefix must be set (use the -p option)" >&2
       return 1
     end
 
-    if test -z $summary
+    if test -z $description
       echo "description must be set (use the -d option)" >&2
       return 1
     end
@@ -29,8 +29,8 @@ function __fd2_define_subcommand_completion -a prefix subcmd summary
       return 1
     end
 
-    fd2_echo "__fd2_define_subcommand_completion $prefix $subcmd" >&2
+    fd2_echo "__fd2_define_subcommand_completion $prefix $command_name" >&2
 
-    complete -x -c "$prefix" -a "$command_name" -d "$summary"
+    complete -x -c "$prefix" -a "$command_name" -d "$description"
 end
 
