20710ec4c18e4ff6709fcb85370b095a086c3059
Andrew Matthews <matthews.andrew@gmail.com>
2020-10-10 16:06:13 +1100

    argparse

--- functions/__fd2_define_command.fish
+++ functions/__fd2_define_command.fish
@@ -1,29 +1,29 @@
 function __fd2_define_command -d "create a command prefix"
-    set -l prefix ''
-    set -l desc ''
 
-    getopts $argv | while read -l key value
-        switch $key
-            case p prefix
-                set prefix $value
-            case d desc
-                set desc $value
-        end
+    set prefix ''
+    set desc ''
+
+    argparse 'p/=+' 'd/=+' -- $argv
+
+    if test -z $_flag_p
+        error "__fd2_define_command: prefix must be set (use the -p option)" >&2
+        return 1
+    else
+        set prefix $_flag_p
     end
 
-    if test -z $prefix
-      error "prefix must be set (use the -p option)" >&2
-      return 1
-    end
-    if test -z $desc
-      error "description must be set (use the -d option)" >&2
-      return 1
+    if test -z $_flag_d
+        error "__fd2_define_command: desc must be set (use the -d option)" >&2
+        return 1
+    else
+        set desc $_flag_d
     end
 
-  trace __fd2_define_command $prefix >&2
-  eval "set -e _subcommand_names_$prefix"
-  if not contains $prefix $_command_names
-      set -U _command_names $_command_names $prefix
-      eval "set -U _command_summary_$prefix '$desc'"
-  end
+
+    trace __fd2_define_command $prefix >&2
+    eval "set -e _subcommand_names_$prefix"
+    if not contains $prefix $_command_names
+        set -U _command_names $_command_names $prefix
+        eval "set -U _command_summary_$prefix '$desc'"
+    end
 end
